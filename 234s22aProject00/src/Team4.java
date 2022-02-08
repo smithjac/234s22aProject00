d45554ce2e242553f9ad938231b32682cad279bc
Devin <mccormackd@moravian.edu>
2022-02-08 09:42:31 -0500

    added devin

--- 234s22aProject00/src/Team4.java
+++ 234s22aProject00/src/Team4.java
@@ -9,6 +9,7 @@ public class Team4 extends Team {
     public void createTeamMembers()
     {
         members.add(new TeamMember("Jacob Smith"));
+        members.add(new TeamMember("Devin McCormack"));
 
         // create a new member class for each team member
         // and add it to the team members list
